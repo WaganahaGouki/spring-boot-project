b714ae96ed8c18b35fde459f868c3d368f27f963
Casian-Gabriel Șuli <casi.suli@gmail.com>
2021-11-05 19:57:04 +0200

    Final project

---
+++ src/main/java/com/example/springbootproject/student/Student.java
@@ -0,0 +1,40 @@
+package com.example.springbootproject.student;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table
+public class Student {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+
+    @Transient
+    private Integer age;
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public Student(String name, String email, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+}
